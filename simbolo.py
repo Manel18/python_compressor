95b31af4933d445db8cb10bec6f9bf6138399eae
Manel18 <peaceandhendrix@gmail.com>
2016-08-23 23:40:07 +0100

    Simbolo class, with a little explanation. Just to lift the spirits :P

--- simbolo.py
+++ simbolo.py
@@ -1,8 +1,21 @@
 class Simbolo:
 	def __init__(self, totalSimb, totalLido):
 		self.__freq = float(totalSimb) / float(totalLido)
-		#self.__byteArray = [];
-		#TODO: See what this class needs to have.
-	
+		self.__code = ""
+		# The object will contain a string of 1's and 0's 
+		# representing the shannon-fano encoding of it, 
+		# we'll then use this in the write section to push
+		# into the buffer this code (not the char '1' or '0')
+		# if code == "0011" and the current state of the buffer
+		# is 10110000 (assuming the buffer is a byte) then
+		# we would have to make those last 4 bits 0011
+		# Thats basically it :P
+
 	def getFreq(self):
 		return self.__freq
+	
+	def getCode(self):
+		return self.__code
+	
+	def appendBit(self, bit):
+		self.__code += bit
